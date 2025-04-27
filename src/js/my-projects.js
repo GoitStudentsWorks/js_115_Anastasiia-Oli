a1da0c1a566d73f566861cf93eb63d366ca539f4
Anastasiia Oliinyk <aolejnik916@gmail.com>
2025-04-27 07:34:26 +0200

    Update my-projects.js 2.3

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -2,8 +2,8 @@ const projects = [
     {
       title: "Wallet webservice",
       technologies: ["React", "JavaScript", "Node JS", "Git"],
-      img1x: "img/my-projects/projects-5@1x.webp",
-      img2x: "img/my-projects/projects-5@2x.webp",
+      img1x: "/src/img/my-projects/projects-5@1x.webp",
+      img2x: "/src/img/my-projects/projects-5@2x.webp",
       link: "https://github.com/Anastasiia-Oli/fur-seal-team-js",
     },
     {
@@ -97,7 +97,7 @@ const projects = [
           <a class="my-projects-link" href="${link}" target="_blank" rel="noopener noreferrer">
             Visit
           <svg class="my-projects-link-icon" width="20" height="20" aria-hidden="true">
-          <use href="/img/sprite.svg#arrow-diagonal"></use>
+          <use href="/src/img/sprite.svg#arrow-diagonal"></use>
           </svg>
           </a>
           </div>
