695f72e8e33a0324b069fbb5643ac5fc1936072a
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-04-28 23:26:29 +0300

    js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,13 @@
-const openBtn = document.getElementById('openModalBtn');
+import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
+
+import { postRequest } from './api'; 
+
+
+
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.button-close');
+const form = document.querySelector('.work-together-form');
 
 function openModal() {
     modalOverlay.classList.add('is-open');
@@ -28,8 +35,6 @@ function handleEscape(event){
     }
 }
 
-openBtn.addEventListener('click', openModal);
-
 closeBtn.addEventListener('click', closeModal);
 
 modalOverlay.addEventListener('click', (event) => {
@@ -37,3 +42,50 @@ modalOverlay.addEventListener('click', (event) => {
         closeModal();
     }
 });
+
+async function handleFormSubmit (event) {
+    event.preventDefault();
+
+ 
+    const email = form.querySelector('[name="email"]').value;
+    const comment = form.querySelector('[name="message"]').value;
+     
+    try {
+        const response = await postRequest(email, comment);  
+        openModal();
+        form.reset();  
+              
+        
+        iziToast.success({
+            title: 'Success',
+            message: 'Form submitted successfully!',
+            position: 'bottomRight',
+            backgroundColor: '#E4E5E6',
+            messageColor: '#292929',
+            titleColor: '#292929',
+        });
+
+    } catch(error){
+        console.log(error);
+
+        iziToast.error({
+            title: 'Error',
+            message: 'Failed to submit form. Please check your data and try again!',
+            position: 'bottomRight',
+            backgroundColor: '#E4E5E6',
+            messageColor: '#292929',
+            titleColor: '#292929',
+            timeout: false,
+        });
+    }
+
+}
+
+form.addEventListener('submit', handleFormSubmit);
+
+export default {
+    openModal,
+    closeModal,
+    handleEscape,
+    handleFormSubmit
+};
