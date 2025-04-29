e11bdd65762e016b45d3e5dbf7930421dce7df79
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-04-29 12:17:18 +0300

    js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,15 @@
-import 'izitoast/dist/css/iziToast.min.css';
-import iziToast from 'izitoast';
-
-import { postRequest } from './api'; 
-
 
 
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.button-close');
-const form = document.querySelector('.work-together-form');
+const modalHeader = document.querySelector('.modal-header');
+const modalText = document.querySelector('.modal-text');
 
-function openModal() {
+
+function openModal({title, message}) {
+    modalHeader.textContent = title;
+    modalText.textContent = message;
+
     modalOverlay.classList.add('is-open');
     document.body.classList.add('no-scroll');
 
@@ -43,49 +43,5 @@ modalOverlay.addEventListener('click', (event) => {
     }
 });
 
-async function handleFormSubmit (event) {
-    event.preventDefault();
 
- 
-    const email = form.querySelector('[name="email"]').value;
-    const comment = form.querySelector('[name="message"]').value;
-     
-    try {
-        const response = await postRequest(email, comment);  
-        openModal();
-        form.reset();  
-              
-        
-        iziToast.success({
-            title: 'Success',
-            message: 'Form submitted successfully!',
-            position: 'bottomRight',
-            backgroundColor: '#E4E5E6',
-            messageColor: '#292929',
-            titleColor: '#292929',
-        });
-
-    } catch(error){
-        console.log(error);
-
-        iziToast.error({
-            title: 'Error',
-            message: 'Failed to submit form. Please check your data and try again!',
-            position: 'bottomRight',
-            backgroundColor: '#E4E5E6',
-            messageColor: '#292929',
-            titleColor: '#292929',
-            timeout: false,
-        });
-    }
-
-}
-
-form.addEventListener('submit', handleFormSubmit);
-
-export default {
-    openModal,
-    closeModal,
-    handleEscape,
-    handleFormSubmit
-};
+export { openModal };
